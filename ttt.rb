f3c894d921a167f2f51a6227815cd6773df0c58e
mswelcome <mattwelcome@gmail.com>
2017-09-26 22:39:19 -0400

    Moves 2 and 3

--- ttt.rb
+++ ttt.rb
@@ -6,5 +6,17 @@ def tttup(player1, choice)
 
 end
 
+def tttup2(player2, choice)
+	board = ["1","2","3","4","5","6","7","8","9"]  
+    board[choice - 1] = player2
+
+    board
+end  
+
+def tttup3(board,player1,choice)  
+    board[choice - 1] = player1
+
+    board
+end
 
 
--- tttt.rb
+++ tttt.rb
@@ -5,14 +5,26 @@ class Test_toe < Minitest::Test
 
 	def test_tttup()
 
-		board = ["1", "2". "3", "4", "5", "6", "7","8", "9"]
-		choice = "2"
-		player1 = x
-		assert_equal(["1","x","3","4","5","6","7","8","9"], tttup(player1,choice))
+		board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+		choice = 2
+		player1 = "x"
+		assert_equal(["1", "x", "3", "4", "5", "6", "7", "8", "9"], tttup(player1,choice))
 
 	end
 
-	
+	def test_tttup_2()
+    	board = ["1","2","3","4","5","6","7","8","9"]
+    	player2 = "o"
+    	choice = 4
+    	assert_equal(["1","2","3","o","5","6","7","8","9"], tttup2(player2,choice))
+	end 
+
+	def test_tttup_3()
+    	board = ["1","x","3","o","5","6","7","8","9"]
+    	player1 = "x"
+    	choice = 8
+    	assert_equal(["1","x","3","o","5","6","7","x","9"], tttup3(board,player1,choice))
+	end 
 
 
 
