81fba0d25978880a03957cc3dc901f9c0e3eefca
mswelcome <mattwelcome@gmail.com>
2017-09-29 01:28:05 -0400

    New Winner function

--- win.rb
+++ win.rb
@@ -2,20 +2,25 @@
 
 def winner()
 	winnums = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-	board = ["x", "x", "x", 3, 4, 5, 6, 7, 8]
+	board = ["x", "x", 3, "o", "o", "o", "7", 8, 9]
 	aryax = []
 	aryao = []
 	count = 0
 
-	while count < 9 
+	while count < 9
 		if board[count] == "x"
 			aryax << count
+		end
+		if board[count] == "o"
+			aryao << count
 		end
 		count += 1
 	end
 
-	puts aryax
-	puts winnums.include?(aryax)
+	winnums.any? do |arya|
+		puts aryax.eql?(arya)
+		puts aryao.eql?(arya)
+	end
 
 end
 
