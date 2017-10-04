809f6107a4dc83dc4726f058e40673e038467d96
mswelcome <mattwelcome@gmail.com>
2017-10-04 06:23:41 -0400

    Console update

--- console.rb
+++ console.rb
@@ -4,3 +4,25 @@ class Console
 
 require_relative 'board.rb'
 require_relative 'seqmove.rb'
+
+
+     attr_accessor :ttt_board
+     attr_accessor :player
+
+     def initialize
+          @ttt_board
+          @player
+     end
+
+     def playgame
+          @player = "x"
+          game = 'run'
+          @ttt_board = [1,2,3,4,5,6,7,8,9]
+
+          until game == 'done' do
+               p '#{ttt_board[0]} || #{ttt_board[1]} || #{ttt_board[2]}'
+               p '= || = || ='
+               p '#{ttt_board[3]} || #{ttt_board[4]} || #{ttt_board[5]}'
+               p '= || = || ='
+               p '#{ttt_board[6]} || #{ttt_board[7]} || #{ttt_board[8]}'
+               p 'Place your marker, pick 1 thru 9.' 
