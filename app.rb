f943d822c6f7b8027f9b121e14b33b70a47108ae
mswelcome <mattwelcome@gmail.com>
2017-10-24 06:41:23 -0400

    syntax corrections

--- app.rb
+++ app.rb
@@ -21,76 +21,73 @@ end
 
 post '/one_player' do
 
-     diff = params[:diff]
+    diff = params[:diff]
 
-     if diff == "Easy"
-          session[:console].player2 = Seqmove.new("o")
-     elsif diff == "TooEasy"
-          session[:console].player2 = Random.new("o")
-     else diff == "VictoryOrDeath"
-          session[:console].player2 = Unai.new("o")
-     end
+    if diff == "Easy"
+        session[:console].player2 = Seqmove.new("o")
+    elsif diff == "TooEasy"
+        session[:console].player2 = Random.new("o")
+    else diff == "VictoryOrDeath"
+        session[:console].player2 = Unai.new("o")
+    end
 
-     redirect '/game'
+    redirect '/game'
 
 end
 
 post '/two_player' do
-     session[:p1name] = params[:p1name]
-     session[:p2name] = params[:p2name]
+    session[:p1name] = params[:p1name]
+    session[:p2name] = params[:p2name]
 
-     session[:console].player1 = Human.new('x')
-     session[:console].player2 = Human.new('o')
-     redirect '/game'
+    session[:console].player1 = Human.new('x')
+    session[:console].player2 = Human.new('o')
+    redirect '/game'
 end
 
 
 get '/game' do
-     choice = params[:choice]
-     msg = params[:msg] || ""
-     session[:ttt_board] = session[:console].board.ttt_board
-     #xp = session[:console].player1.marker
-     #op = session[:console].player2.marker
+    msg = params[:msg] || ""
+    session[:ttt_board] = session[:console].board.ttt_board
+    #xp = session[:console].player1.marker
+    #op = session[:console].player2.marker
 
 
-     erb :game, locals: {ttt_board: session{:ttt_board},msg: msg,choice: choice}
+    erb :game, locals: {ttt_board: session{:ttt_board},msg: msg,choice: choice}
 end
 
 post '/loop' do
-     choice = params[:choice]
+    choice = params[:choice]
 
     if session[:console].board.open_spot?(session[:ttt_board],choice) == true
-         session[:console].board.tttup(session[:ttt_board],choice,session[:console].player1.marker)
-         #session[:console].cp = session[:console].player2
-     else redirect '/game?msg=Invalid choice' + '&choice=' + choice
-     end
+        session[:console].board.tttup(session[:ttt_board],choice,session[:console].player1.marker)
+    else redirect '/game?msg=Invalid choice'
+    end
 
-     if session[:console].board.winner(session[:ttt_board]) || session[:console].board.fullboard?(session[:ttt_board])
+    if session[:console].board.winner(session[:ttt_board]) || session[:console].board.fullboard?(session[:ttt_board])
           redirect '/gameresults'
-     else
-          if session[:console].player2 != Human.new('o')
-               choice = session[:console].cp.getmove(session[:ttt_board])
-               session[:console].board.tttup(session[:ttt_board],choice,session[:console].player2.marker)
-               #session[:console].cp = session[:console].player1
-          else
-               if session[:console].board.open_spot?(session[:ttt_board],choice) == true
-                 session[:console].board.tttup(session[:ttt_board],choice,session[:console].player2.marker)
-                 #session[:console].cp = session[:console].player2
-               else redirect '/game?msg=Invalid choice' + '&choice=' + choice
-               end
-          end
-     end
+    else
+        if session[:console].player2 == Human.new('o')
+            if session[:console].board.open_spot?(session[:ttt_board],choice) == true
+                session[:console].board.tttup(session[:ttt_board],choice,session[:console].player2.marker)
+            else redirect '/game?msg=Invalid choice' + '&choice=' + choice
+            end
+        else
+            choice = session[:console].cp.getmove(session[:ttt_board])
+            session[:console].board.tttup(session[:ttt_board],choice,session[:console].player2.marker)
+        end
+    end
 
 
-  if session[:console].board.winner(session[:ttt_board]) || session[:console].board.fullboard?(session[:ttt_board])
-    redirect '/gameresults'
-  else redirect '/game'
-  end
+    if session[:console].board.winner(session[:ttt_board]) || session[:console].board.fullboard?(session[:ttt_board])
+        redirect '/gameresults'
+    else redirect '/game'
+    end
 
 end
 
 get '/gameresults' do
 
-     erb :gameresults, locals: {ttt_board: session[:ttt_board]}
+
+    erb :gameresults, locals: {ttt_board: session[:ttt_board]}
 
 end
